7f0f5258120b065f5c01c0d630e8f02c8ddf1111
Frank Sun <popsun2010@gmail.com>
2015-10-17 09:28:41 -0700

    Add show user's profile feature and test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,10 @@ class UsersController < ApplicationController
       render "/users/new"
     end
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
 private 
   def user_params
    params.require(:user).permit(:first_name, :last_name, :email, :password)
--- spec/features/show_user_spec.rb
+++ spec/features/show_user_spec.rb
@@ -1,12 +1,12 @@
-# require 'rails_helper'
-# RSpec.describe 'user profile page' do
-#   it 'displays information about the user' do
-#     user = User.create(first_name: 'Dexter', last_name: 'Clark', email: 'dexter@gmail.com', password: 'password')
-#     visit '/users'
-#     click_link 'Show'
-#     expect(current_path).to eq("/users/#{user.id}")
-#     expect(page).to have_text('Dexter')
-#     expect(page).to have_text('Clark')
-#     expect(page).to have_text('dexter@gmail.com')
-#   end
-# end
+require 'rails_helper'
+RSpec.describe 'user profile page' do
+  it 'displays information about the user' do
+    user = User.create(first_name: 'Dexter', last_name: 'Clark', email: 'dexter@gmail.com', password: 'password')
+    visit '/users'
+    click_link 'Show'
+    expect(current_path).to eq("/users/#{user.id}")
+    expect(page).to have_text('Dexter')
+    expect(page).to have_text('Clark')
+    expect(page).to have_text('dexter@gmail.com')
+  end
+end
